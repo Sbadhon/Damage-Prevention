59c156d587a7a2d5bbe69057ae20ff58a383d074
Sbadhon <sohana89@yahoo.com>
2025-12-02 18:36:48 -0600

    Fix solution projects for CI

--- src/Gateway/obj/Debug/net9.0/Gateway.AssemblyInfo.cs
+++ src/Gateway/obj/Debug/net9.0/Gateway.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Gateway")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dd8384fdd1dff8e18012090348c8a12806a333e5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+807a1bf4d0cee9e1529e3359137ad767d05ea888")]
 [assembly: System.Reflection.AssemblyProductAttribute("Gateway")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Gateway")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
